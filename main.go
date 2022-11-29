d49fdad1e9aa392d79c4e40758154b753d690df6
Ryano0oceros <getryanfallon@gmail.com>
2022-11-29 07:44:34 -0500

    after 41

--- main.go
+++ main.go
@@ -2,17 +2,27 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
-	var mf person
-	fmt.Printf("%+v", mf)
-
-	mf.firstName = "King"
-	mf.lastName = "Geedorah"
+	mf := person{
+		firstName: "MF",
+		lastName:  "DOOM",
+		contact: contactInfo{
+			email:   "MF@DOOM",
+			zipCode: 33333,
+		},
+	}
 	fmt.Println(mf.firstName)
 	fmt.Println(mf.lastName)
+	fmt.Println(mf.contact)
 }
